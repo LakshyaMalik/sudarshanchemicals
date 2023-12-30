0970043434f02cdc47e384650dee1bd25e32f8d8
LakshyaMalik <ls1563@srmist.edu.in>
2023-12-31 04:11:15 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,8 +28,8 @@ export const Navbar = () => {
                     <li><Link to="/contact">Contact Us</Link></li>
                     <li><Link to="/enquiry">Enquiry</Link></li>
                     <li id="social">
-                        <h4><a href="https://www.facebook.com/profile.php?id=100010836252495" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
-                        <a href="https://www.instagram.com/sanjeev7856/?utm_source=ig_web_button_share_sheet&igshid=MmVlMjlkMTBhMg==" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></h4>
+                        <h4><a href="https://www.facebook.com/profile.php?id=100010836252495" target="_blank" rel="noopener noreferrer"><SlSocialFacebook /></a>
+                        <a href="https://www.instagram.com/sanjeev7856/?utm_source=ig_web_button_share_sheet&igshid=MmVlMjlkMTBhMg==" target="_blank" rel="noopener noreferrer"><SiInstagram /></a></h4>
                     </li>
                 </ul>
             </div>
